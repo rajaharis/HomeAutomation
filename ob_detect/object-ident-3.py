88a137dca2014effdb31c73840b13fe18dad242c
rajaharis <harisr426@gmail.com>
2022-07-23 12:17:31 +0500

    all done

---
+++ ob_detect/object-ident-3.py
@@ -0,0 +1,77 @@
+import cv2
+
+from flask import Flask, Response
+app = Flask(__name__)
+
+
+classNames = []
+classFile = "/home/haris/Desktop/Object_Detection_Files/coco.names"
+with open(classFile,"rt") as f:
+    classNames = f.read().rstrip("\n").split("\n")
+
+configPath = "/home/haris/Desktop/Object_Detection_Files/ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt"
+weightsPath = "/home/haris/Desktop/Object_Detection_Files/frozen_inference_graph.pb"
+
+net = cv2.dnn_DetectionModel(weightsPath,configPath)
+net.setInputSize(320,320)
+net.setInputScale(1.0/ 127.5)
+net.setInputMean((127.5, 127.5, 127.5))
+net.setInputSwapRB(True)
+
+
+def getObjects(img, thres, nms, draw=True, objects=[]):
+    classIds, confs, bbox = net.detect(img,confThreshold=thres,nmsThreshold=nms)
+    #print(classIds,bbox)
+    if len(objects) == 0: objects = classNames
+    objectInfo =[]
+    if len(classIds) != 0:
+        for classId, confidence,box in zip(classIds.flatten(),confs.flatten(),bbox):
+            className = classNames[classId - 1]
+            if className in objects: 
+                objectInfo.append([box,className])
+                if (draw):
+                    cv2.rectangle(img,box,color=(0,255,0),thickness=2)
+                    cv2.putText(img,classNames[classId-1].upper(),(box[0]+10,box[1]+30),
+                    cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
+                    cv2.putText(img,str(round(confidence*100,2)),(box[0]+200,box[1]+30),
+                    cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
+                    
+                    # servo.angle = -90
+                    # time.sleep = 2
+                    # servo.angle = 90
+    
+    return img,objectInfo
+
+
+
+
+def video():   
+    
+    cap = cv2.VideoCapture(0)
+    cap.set(3,640)
+    cap.set(4,480)
+    cap.set(10,70)
+    while True:
+        # success, img = cap.read()
+        #print(objectInfo)
+
+       
+        success, frame = cap.read()
+        result, objectInfo = getObjects(frame,0.45,0.2, objects=classNames)
+
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+        yield(b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame+ b'\r\n')
+        
+        
+        # cv2.imshow("Output",result)
+        # cv2.waitKey(10)
+@app.route('/video')
+def videoStream():
+    return Response(video(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == "__main__":
+ app.run(host=('0.0.0.0'),port=5000)
